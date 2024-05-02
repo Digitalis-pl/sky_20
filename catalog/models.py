51d87c0a1880dc9e9b65e1b7a0c8459f2065264d
georg <zhora.karsakov@mail.ru>
2024-05-02 15:42:37 +0300

    3я миграция

--- catalog/models.py
+++ catalog/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 
 # Create your models here.
+null_options = {'blank': True, 'null': True}
 
 
 class Product(models.Model):
-    name = models.ForeignKey('Category', on_delete=models.SET_NULL, blank=True, null=True)
-    description = models.TextField
+    name = models.ForeignKey('Category', on_delete=models.SET_NULL, **null_options)
+    description = models.TextField(verbose_name='описание', **null_options)
     img = models.ImageField(upload_to='product/img')
     category = models.CharField(max_length=100)
-    price = models.IntegerField
-    created_at = models.DateField
-    updated_at = models.DateField
-    manufactured_at = models.DateField(verbose_name='дата производства', blank=True, null=True)
+    price = models.IntegerField(verbose_name='цена', **null_options)
+    created_at = models.DateField(verbose_name='дата создания', **null_options)
+    updated_at = models.DateField(verbose_name='дата последнего изменения', **null_options)
 
     def __str__(self):
         return f'{self.name} {self.description} {self.category} {self.price}'
@@ -23,7 +23,7 @@ class Product(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField
+    description = models.TextField(verbose_name='описание', **null_options)
 
     def __str__(self):
         return f'{self.name} {self.description}'
