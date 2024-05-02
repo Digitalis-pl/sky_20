215f90094905f6781e84db29442604dd9188d784
georg <zhora.karsakov@mail.ru>
2024-05-02 15:11:48 +0300

    1я миграция

---
+++ catalog/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Product(models.Model):
+    name = models.ForeignKey('Category', on_delete=models.SET_NULL, blank=True, null=True)
+    description = models.TextField
+    img = models.ImageField(upload_to='product/img')
+    category = models.CharField(max_length=100)
+    price = models.IntegerField
+    created_at = models.DateField
+    updated_at = models.DateField
+
+    def __str__(self):
+        return f'{self.name} {self.description} {self.category} {self.price}'
+
+    class Meta:
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField
+
+    def __str__(self):
+        return f'{self.name} {self.description}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
