3ed5d901818a8f1a13c6615fc2194034d81706c2
georg <zhora.karsakov@mail.ru>
2024-05-02 15:24:07 +0300

    2я миграция

--- catalog/models.py
+++ catalog/models.py
@@ -11,6 +11,7 @@ class Product(models.Model):
     price = models.IntegerField
     created_at = models.DateField
     updated_at = models.DateField
+    manufactured_at = models.DateField(verbose_name='дата производства', blank=True, null=True)
 
     def __str__(self):
         return f'{self.name} {self.description} {self.category} {self.price}'
