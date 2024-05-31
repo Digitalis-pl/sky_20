bd8f43526f2a6e6a482470631582324f5977c94a
georg <zhora.karsakov@mail.ru>
2024-05-31 13:50:55 +0300

    работа 22.1

---
+++ catalog/forms.py
@@ -0,0 +1,47 @@
+from django.forms import ModelForm, BooleanField
+from django.core.exceptions import ValidationError
+from catalog.models import Product, Version
+
+
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for fild_name, fild in self.fields.items():
+            if isinstance(fild, BooleanField):
+                fild.widget.attrs['class'] = "form-check-input"
+            else:
+                fild.widget.attrs['class'] = "form-control"
+
+
+class ProductForm(StyleFormMixin, ModelForm):
+    class Meta:
+        fields = '__all__'
+        model = Product
+
+    def clean(self):
+        bad_words = {'казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар'}
+        name = set(self.cleaned_data['name'].split(" "))
+        description = set(self.cleaned_data['description'].split(" "))
+        a = name & bad_words
+        b = description & bad_words
+        if len(a) > 0 or len(b) > 0:
+            print('some')
+            raise ValidationError(f'Использование слов {bad_words}, запрещено')
+
+
+class VersionForm(StyleFormMixin, ModelForm):
+    class Meta:
+        exclude = ('error_message',)
+        model = Version
+
+    def clean(self):
+        flags = []
+        obj_list = Version.objects.all()
+        for obj in obj_list:
+            flags.append(obj.version_sign)
+        print(flags)
+        sign = self.cleaned_data['version_sign']
+        if sign is True and True in flags:
+            raise ValidationError(f'может быть только одна активная версия')
+
+        return sign
--- catalog/urls.py
+++ catalog/urls.py
@@ -2,18 +2,29 @@ from django.urls import path
 from catalog.apps import CatalogConfig
 from catalog.views import (main_page, ProductView, OneProductView,
                            CreateContactView, ContactView,
-                           CraeteProductView, BlogView,
+                           CreateProductView, BlogView,
                            BlogDetailView, BlogCreateView,
-                           BlogUpdateView, BlogDeleteView)
+                           BlogUpdateView, BlogDeleteView,
+                           DeleteProduct, CreateVersionView,
+                           VersionListView, VersionUpdateView,
+                           activity_button, CategoryListView,
+                           VersionDeleteView,)
 
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', main_page),
+    path('category/', CategoryListView.as_view(), name='category'),
+    path('activity/<int:pk>', activity_button, name='activity'),
     path('contacts/', ContactView.as_view(), name='contacts'),
+    path('add_version/', CreateVersionView.as_view(), name='add_version'),
+    path('versions/<int:pk>', VersionListView.as_view(), name='versions'),
+    path('version_update/<int:pk>', VersionUpdateView.as_view(), name='update_version'),
+    path('delete_version/<int:pk>', VersionDeleteView.as_view(), name='version_delete'),
     path('products/', ProductView.as_view(), name='product_page'),
-    path('add_prod/', CraeteProductView.as_view(), name='add_prod'),
+    path('add_prod/', CreateProductView.as_view(), name='add_prod'),
+    path('delete_product/<int:pk>', DeleteProduct.as_view(), name='delete_product'),
     path('our_product/<int:pk>', OneProductView.as_view(), name='one_product'),
     path('add_contact/', CreateContactView.as_view(), name='addcontact'),
     path('blog_main/', BlogView.as_view(), name='blog_main'),
